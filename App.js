901d462faaddd519dd5f3244df87ec36d74cadff
Matt Clark <matt.clark@MClark-L281.local>
2023-06-08 13:29:18 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PagerView from 'react-native-pager-view';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <PagerView style={styles.pagerView} orientation="vertical" initialPage={0} >
+        <View key="1" style={styles.pageOne}>
+          <Text>First Page</Text>
+        </View>
+        <View key="2" style={styles.pageTwo}>
+          <Text>Second Page</Text>
+        </View>
+        <View key="3" style={styles.pageThree}>
+          <Text>Second Page</Text>
+        </View>
+
+      </PagerView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    pageOne:{
+        backgroundColor: "blue",
+    },
+    pageTwo:{
+        backgroundColor: "red",
+    },
+    pageThree:{
+        backgroundColor: "green",
+    },
+
+    pagerView:{
+      flex: 1
+    },
 });
